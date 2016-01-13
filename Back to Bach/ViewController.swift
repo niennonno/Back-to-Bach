fb10a703889e17ca70db86b82857b215459247fc
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2016-01-13 19:01:20 +0530

    Finished
    App displays the working of both the scrubs

--- Back to Bach/ViewController.swift
+++ Back to Bach/ViewController.swift
@@ -7,38 +7,76 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
 
     @IBOutlet var volumeSlider: UISlider!
     
+    @IBOutlet var scrubSlider: UISlider!
     
     @IBAction func play(sender: AnyObject) {
         
-        
+        player.play()
         
     }
     
     @IBAction func pause(sender: AnyObject) {
         
-        
+        player.pause()
         
     }
     
     @IBAction func stop(sender: AnyObject) {
         
-        
+        player.stop()
+    
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("K 545", ofType: "mp3")!))
+            
+        }catch {
+            print("Error!")
+        }
         
     }
     
     @IBAction func adjustVolume(sender: AnyObject) {
         
+        player.volume = volumeSlider.value
         
+    }
+    
+    @IBAction func scrub(sender: AnyObject) {
+        
+        player.currentTime = Double(scrubSlider.value)
+        
+    }
+    
+    func updateScrubSlider(){
+        
+        scrubSlider.value = Float(player.currentTime)
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("K 545", ofType: "mp3")!))
+            
+            scrubSlider.maximumValue = Float(player.duration)
+        
+        }catch {
+            print("Error!")
+        }
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateScrubSlider"), userInfo: nil, repeats: true)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
