297a02096e331e5e56d18839085dd48e23ebd14f
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2016-01-13 18:29:48 +0530

    Layout

---
+++ Back to Bach/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Back to Bach
+//
+//  Created by Aditya Vikram Godawat on 13/01/16.
+//  Copyright © 2016 Wow Labz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var volumeSlider: UISlider!
+    
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
